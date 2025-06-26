645f6d9f0e9ca21cb424832523deb7e454f91c92
Jaswanth Madiya <jaswanthmadiya@gmail.com>
2025-06-26 22:07:38 +0530

    Database Configuration Updated

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -79,16 +79,22 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'littlelemon_db',
-        'USER': 'admin',
-        'PASSWORD': 'admin@123!',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'littlelemon_db',
+    #     'USER': 'admin',
+    #     'PASSWORD': 'admin@123!',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '3306',
+    #     'OPTIONS': {
+    #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+    #     }
+    # }
+    #
 }
 
 REST_FRAMEWORK = {
